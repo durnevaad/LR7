e9d1bf3b709c2f065d27d1fa2895e5b83bfe20f0
durnevaad <93126385+durnevaad@users.noreply.github.com>
2021-11-04 16:30:56 +0500

    Навигация для сайта

---
+++ navigat.php
@@ -0,0 +1,13 @@
+<title>Дурнева А.Д.</title>
+<ul>Дурнева А.Д. ЭАС-310</ul>
+<ul>Задания:</ul>
+<li><a href="Ex9.html">Задание 1</a></li>
+<li><a href="Ex10.html">Задание 2</a></li>
+<li><a href="Ex11.html">Задание 3</a></li>
+<li><a href="Ex12.html">Задание 4</a></li>
+<li><a href="Ex13.html">Задание 5</a></li>
+<li><a href="Ex14.html">Задание 6</a></li>
+<li><a href="Ex15.html">Задание 7</a></li>
+<li><a href="Ex16.html">Задание 8</a></li>
+<li><a href="Ex17.html">Задание 9</a></li>
+<p><a href="../index.php">Вернуться назад</a></p>
